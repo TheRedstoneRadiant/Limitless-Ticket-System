f93a56f7f08c0dff4bc20d16befd11aa7ed2c83a
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2022-11-22 18:58:32 -0500

    Remove ununsed express code

--- index.js
+++ index.js
@@ -29,15 +29,3 @@ client.on("guildMemberAdd", async (member) => {
 
 	channel.send("@everyone");
 });
-
-// const app = express();
-// app.set("view engine", "ejs");
-// app.use(express.static("public"));
-
-// app.get("/", (req, res) => {
-//     res.render("index");
-// });
-
-// app.listen(8080, () => {
-//     console.log("Listening on port 8080\nhttp://localhost:8080");
-// });
